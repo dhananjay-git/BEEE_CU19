2e22e618c03707a089f5c3870cec9edfe60a5ac9
dhananjay-git <55528456+dhananjay-git@users.noreply.github.com>
2019-09-21 17:30:46 +0530

    Add files via upload

---
+++ EXP1_LED_FLASHER/led_flasher.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  for (int i=5; i<8; i++)
+  	pinMode(i,OUTPUT);
+}
+
+void loop()
+{
+  for (int i=5; i<8; i++)
+  	digitalWrite(i,HIGH);
+  delay(200);
+  for (int i=5; i<8; i++)
+  	digitalWrite(i,LOW);
+  delay(200);
+}
